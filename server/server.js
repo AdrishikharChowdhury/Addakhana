c516570bf428ae1ca1f118350af3a3c1a8bc1ce7
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-04-26 21:06:34 +0530

    CORS updated again

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://addakhana-pied.vercel.app/" },
 });
 
 export const userSocketMap = {};
