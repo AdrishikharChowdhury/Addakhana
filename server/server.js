65d38117df510aeecbfdd515a23e2756ecd1ab0a
AdrishikharChodhury <amiadrishikhar@gmail.com>
2026-04-26 20:56:48 +0530

    NODE_ENV added

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" }
+  cors: { origin: "http://localhost:5173" },
 });
 
 export const userSocketMap = {};
@@ -48,8 +48,12 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  log(`Server is running on PORT: ${PORT}`);
-});
+  server.listen(PORT, () => {
+    log(`Server is running on PORT: ${PORT}`);
+  });
+}
+
+export default server
